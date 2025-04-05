f712ee495189697575d3d9856ad5c9075c3576c1
akmbaziira <akmbaziira@AKMBAZIIRAD18E>
2025-04-05 05:04:16 +0300

    Add project files.

---
+++ HTMLHelpers/Controllers/The_UserController.cs
@@ -0,0 +1,29 @@
+﻿using HTMLHelpers.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLHelpers.Controllers
+{
+    public class The_UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Countries = new List<string> { "USA", "Canada", "Germany", "UK", "India", "Australlia" };
+            ViewBag.Skills = new List<string> { "CSharp", "JS", "Python", "Java", "PHP" };
+
+            return View(new User());
+        }
+
+        [HttpPost]
+        public ActionResult submit(User user)
+        {
+            if (ModelState.IsValid) {
+                return View("Success", User);
+            }
+            ViewBag.Countries = new List<string> { "USA", "Canada", "Germany", "UK", "India", "Australlia" };
+            ViewBag.Skills = new List<string> { "CSharp", "JS", "Python", "Java", "PHP" };
+
+            return View("Index", user);
+        }
+    }
+}
---
+++ HTMLHelpers/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace HTMLHelpers.Models
+{
+    public class User
+    {
+        [Display(Name = "Full Name")]
+        public String? Name { get; set; }
+        
+        [Display(Name = "Email Address")]
+
+        public String? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        public string? Gender { get; set; }
+        public bool Issubscribed { get; set; }
+        public String? Country { get; set; }
+        public  String? Skills { get; set; }
+
+    }
+}
